a4661855b81d4a2cbfce7722addc29a8efbc8daf
phizura <satrioapra@gmail.com>
2025-03-13 10:26:15 +0700

    feat: Register AuthRefresh middleware for token refresh handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\AuthRefresh;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -17,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'auth.token' => Authenticate::class,
+            'auth.refresh' => AuthRefresh::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
